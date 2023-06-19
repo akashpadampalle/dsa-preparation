fd3f35cc36600c1bad605bf7a12aebb7815359c6
Akash Padampalle <akashpadampalle780@gmail.com>
2023-06-19 10:49:35 +0530

    add selection sort

--- sorting algo/Main.java
+++ sorting algo/Main.java
@@ -1,9 +1,11 @@
 public class Main{
 
+    // bubble sort with one argument
     public static void bubbleSort(int[] nums){
         bubbleSort(nums, false);
     }
 
+    // bubble sort
     public static void bubbleSort(int[] nums, boolean isDescending){
         int length = nums.length;
 
@@ -14,7 +16,7 @@ public class Main{
         for(int i = length - 1 ; i >= 0; i--){
             for(int j = 0 ; j < i ; j++){
                 
-                // 
+                // handle both the case is accending and descending scenarios
                 if((nums[j] > nums[j+1] && !isDescending) || (nums[j] < nums[j+1] && isDescending)){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
@@ -25,6 +27,33 @@ public class Main{
 
     }
 
+
+    //selection sort with one argument
+    public static void selectionSort(int[] nums){
+        selectionSort(nums, false);
+    }
+
+    // selection sort
+    public static void selectionSort(int[] nums, boolean isDescending){
+        for(int i = 0 ; i < nums.length; i++){
+
+            // finding maximum element in unsorted array
+            int target = i;
+            for(int j = i + 1; j < nums.length; j++){
+                if((nums[target] > nums[j] && !isDescending) || (nums[target] < nums[j] && isDescending)){
+                    target = j;
+                }
+            }
+
+            // swapping max element with position of ith element
+            int temp = nums[i];
+            nums[i] = nums[target];
+            nums[target] = temp;
+
+        }
+    }
+
+    // print array
     public static void print(int[] nums){
         System.out.print("[ ");
         for(int i = 0 ; i < nums.length; i++){
@@ -34,10 +63,12 @@ public class Main{
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 5, 2, 8, 6, 4};
+        int[] nums = { 1, 5, 0, 2, 8, 6, 4};
 
         print(nums);
-        bubbleSort(nums);
+        bubbleSort(nums, true);
         print(nums);
+        // selectionSort(nums);
+        // print(nums);
     }
 }
